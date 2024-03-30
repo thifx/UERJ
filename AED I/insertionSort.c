cda517ecb5d001b55eebd0c685f270a2430aac0f
Thiago G Ribeiro <thiagogr71@gmail.com>
2024-03-30 20:49:38 -0300

    feat: implement insertion sort

---
+++ AED I/insertionSort.c
@@ -0,0 +1,22 @@
+#include "printarray.h"
+#include "readarray.h"
+
+int main()
+{
+    int array_size;
+    int *userArray = read_array(&array_size);
+
+    for (int i = 1; i < array_size; i++)
+    {
+        int key = userArray[i];
+        int j = i - 1;
+        while (j >= 0 && key < userArray[j])
+        {
+            userArray[j + 1] = userArray[j];
+            j--;
+        }
+        userArray[j + 1] = key;
+    }
+    print_array(userArray, array_size);
+    return 0;
+}
