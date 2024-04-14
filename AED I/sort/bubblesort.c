01c79fd44d084c1e368f22102e4fc5dfd3b09189
Thiago Ribeiro <thiagogr71@gmail.com>
2024-04-14 19:22:07 -0300

    implement bubble sort

--- AED I/sort/bubblesort.c
+++ AED I/sort/bubblesort.c
@@ -6,6 +6,18 @@ int main()
     int array_size;
     int *userArray = read_array(&array_size);
 
+    for (int i = array_size - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (userArray[j] > userArray[j + 1])
+            {
+                int tmp = userArray[j + 1];
+                userArray[j + 1] = userArray[j];
+                userArray[j] = tmp;
+            }
+        }
+    }
     print_array(userArray, array_size);
     return 0;
 }
