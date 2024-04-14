4e63c008b1ffd20cb324a20668220c4b2aea8a90
Thiago Ribeiro <thiagogr71@gmail.com>
2024-04-14 18:45:18 -0300

    avoid overflow in binary search

--- AED I/search/binarySearch.c
+++ AED I/search/binarySearch.c
@@ -10,25 +10,24 @@ int main()
     puts("Type the desired number:");
     scanf("%d", &desired);
     int mid = 0;
-    int c = 0;
-    int f = array_size - 1;
-    while (c <= f)
+    int lo = 0;
+    int hi = array_size - 1;
+    while (lo <= hi)
     {
-        mid = (c + f) / 2;
+        mid = lo + (hi - lo) / 2;
         if (user_array[mid] > desired)
         {
-            f = mid - 1;
+            hi = mid - 1;
         }
         else if (user_array[mid] < desired)
         {
-            c = mid + 1;
+            lo = mid + 1;
         }
         else
         {
-            printf("found at index: %d", mid);
+            printf("found at index: %d\n", mid);
             return 0;
         }
-        printf("i: %d;c: %d;f: %d\n", mid, c, f);
     }
     return 0;
 }
